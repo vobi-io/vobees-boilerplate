f7aa49fe38a6d2addd26f35f52bc9ca64bf7a0dd
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2020-04-23 10:11:24 +0400

    fix: config file

--- src/config/default.js
+++ src/config/default.js
@@ -2,6 +2,6 @@
 module.exports = {
   port: 8000,
   database: {
-    connection: 'mongodb://localhost:27017/invoicewave?retryWrites=true'
+    connection: 'mongodb://localhost:27017/demo?retryWrites=true'
   }
 }
